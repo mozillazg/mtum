f51bdf31f3e336980b399ab92865b6afee3ce707
Mozillazg <opensource.mozillazg@gmail.com>
2013-02-23 17:57:51 +0800

    Let Decorator support optional keyword arguments.

--- mtum/post/utils.py
+++ mtum/post/utils.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from functools import wraps
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
+from django.http import HttpResponseRedirect
 
 
-# decorator
-def object_does_not_exist(func):
-    def returned_wrapper(request, *args, **kwargs):
-        try:
-            return func(request, *args, **kwargs)
-        except ObjectDoesNotExist:
-            raise Http404()
-    return returned_wrapper
+def object_does_not_exist(view_func=None, redirect=None):
+    """Decorator for views that catch ObjectDoesNotExist Exception.
+    if redirect is None, raise Http404 Exception, otherwise Redirect.
+    """
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            try:
+                return view_func(request, *args, **kwargs)
+            except ObjectDoesNotExist:
+                if redirect:
+                    return HttpResponseRedirect(redirect)
+                else:
+                    raise Http404()
+        return _wrapped_view
+
+    if not view_func:
+        def foo(view_func):
+            return decorator(view_func)
+        return foo
+
+    else:
+        return decorator(view_func)

c735c85ddaec7ad31866029368d5f7cdc7a9d3bc
Mozillazg <opensource.mozillazg@gmail.com>
2013-02-25 16:42:38 +0800

    Adjusted admin pages.

--- mtum/post/admin.py
+++ mtum/post/admin.py
@@ -8,7 +8,23 @@ from .models import Post
 from .models import Like
 from .models import Follow
 
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post')
+    list_filter = ('author',)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('follower', 'following')
+    list_filter = ('follower', 'following')
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'kind', 'reblog', 'created_at')
+    search_fields = ('title', 'content')
+    list_filter = ('created_at', 'author')
+
 admin.site.register(Tag)
-admin.site.register(Post)
-admin.site.register(Like)
-admin.site.register(Follow)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Like, LikeAdmin)
+admin.site.register(Follow, FollowAdmin)

58a58c1371a861ab195aa392cecae73159bb6aeb
Mozillazg <opensource.mozillazg@gmail.com>
2013-02-25 11:46:43 +0800

    Added error message for settings page, when e-mail address already exists.

--- mtum/dashboard/views_settings.py
+++ mtum/dashboard/views_settings.py
@@ -6,39 +6,51 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse_lazy
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 from .forms import AccountForm
 
 
 @login_required(login_url=reverse_lazy('login'))
-def account(request):
+def account(request, template_name='settings/index.html'):
     user = request.user
     profile = user.get_profile()
-    errors = None
+    errors = {}
     if request.method == 'POST':
         form = AccountForm(request.POST)
         if form.is_valid():
-            title = form.cleaned_data['title']
-            description = form.cleaned_data['description']
-            email = form.cleaned_data['email']
-            current_password = form.cleaned_data['c_password']
-            new_password = form.cleaned_data['n_password']
-            cm_new_password = form.cleaned_data['m_password']
+            try:
+                title = form.cleaned_data['title']
+                description = form.cleaned_data['description']
+                email = form.cleaned_data['email']
+                current_password = form.cleaned_data['c_password']
+                new_password = form.cleaned_data['n_password']
+                cm_new_password = form.cleaned_data['m_password']
 
-            if title != profile.title:
-                profile.title = title
-            if description != profile.description:
-                profile.description = description
-            if email != user.email:
-                user.email = email
-            if (user.check_password(current_password) and new_password
-                    and new_password == cm_new_password):
-                user.set_password(new_password)
+                if title != profile.title:
+                    profile.title = title
+                if description != profile.description:
+                    profile.description = description
 
-            user.save()
-            profile.save()
+                if email != user.email:
+                    if User.objects.filter(email=email).exists():
+                        errors_msg_email = '<ul><li class="error">'
+                        errors_msg_email += 'This e-mail address already'
+                        errors_msg_email += ' exists!</li></ul>'
+                        errors['email'] = errors_msg_email
+                        raise Exception()
+                    user.email = email
 
-            return HttpResponseRedirect(reverse_lazy('settings'))
+                if (user.check_password(current_password) and new_password
+                        and new_password == cm_new_password):
+                    user.set_password(new_password)
+
+                user.save()
+                profile.save()
+
+                return HttpResponseRedirect(reverse_lazy('settings'))
+            except:
+                pass
         else:
             errors = form.errors
     else:
@@ -55,5 +67,5 @@ def account(request):
         'kind': 'account',
         'errors': errors,
     }
-    return render_to_response('settings/index.html', context,
+    return render_to_response(template_name, context,
                               context_instance=RequestContext(request))

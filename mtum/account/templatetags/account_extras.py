96ccac7f0ca3a31ccc559377c6a8de49f5c1711e
Mozillazg <opensource.mozillazg@gmail.com>
2013-03-13 10:14:38 +0800

    Changed default avatar url.

--- mtum/account/templatetags/account_extras.py
+++ mtum/account/templatetags/account_extras.py
@@ -7,15 +7,15 @@ register = template.Library()
 def get_avatar(user, size, default=None):
     if not default:
         if size == 128:
-            default = 'http://assets.tumblr.com/images/default_avatar_128.gif'
+            default = 'http://tumblr.3sd.me/static/img/default_avatar_128.gif'
         elif size == 64:
-            default = 'http://assets.tumblr.com/images/default_avatar_64.gif'
+            default = 'http://tumblr.3sd.me/static/img/default_avatar_64.gif'
         elif size == 30:
-            default = 'http://assets.tumblr.com/images/default_avatar_30.gif'
+            default = 'http://tumblr.3sd.me/static/img/default_avatar_30.gif'
         elif size == 40:
-            default = 'http://assets.tumblr.com/images/default_avatar_40.gif'
+            default = 'http://tumblr.3sd.me/static/img/default_avatar_40.gif'
         elif size == 16:
-            default = 'http://assets.tumblr.com/images/default_avatar_16.gif'
+            default = 'http://tumblr.3sd.me/static/img/default_avatar_16.gif'
 
     avatar_url = user.get_profile().get_avatar(size, default)
     return avatar_url
